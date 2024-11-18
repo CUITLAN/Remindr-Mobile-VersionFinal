002fe05ea85095f7b40b2d4d8e89a3c71e364cba
Cuitlan <alan78707@gmail.com>
2024-11-17 20:36:46 -0600

    Added Screen: LoginScreen

---
+++ assets/Botones/ButtonPrimary.jsx
@@ -0,0 +1,24 @@
+import { Button } from 'native-base';
+import React from 'react';
+
+const ButtonPrimary = ({ text, size = "md", onPress }) => {
+  return (
+    <Button
+      mt="4"
+      size={size}
+      bg="#D900FF"
+      _pressed={{ bg: "#b260ff" }}
+      _text={{
+        color: "white",
+        fontWeight: "400",
+        fontFamily: "Itim",
+        fontSize: 16,
+      }}
+      onPress={onPress} 
+    >
+      {text}
+    </Button>
+  );
+};
+
+export default ButtonPrimary;
