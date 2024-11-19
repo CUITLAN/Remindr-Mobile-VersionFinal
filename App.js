b8a9b4cb2fc3199cb2f960078877ed16f500677f
Cuitlan <alan78707@gmail.com>
2024-11-18 23:30:24 -0600

    Una prueba de rendimiento

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ export default function App() {
   //  <NativeBaseProvider theme={theme}>
   //    <DrawerNavigation />
   // </NativeBaseProvider>
-   //  <LoginSignUp/>
-   <Login/>
+    <LoginSignUp/>
+  //  <Login/>
   );
 }
